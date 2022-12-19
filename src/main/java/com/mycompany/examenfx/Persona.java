5e836efac9646cab7b58e02d3b49d37593dd6202
Alex5mb <92161024+Alex5mb@users.noreply.github.com>
2022-12-19 18:14:37 +0100

    Examen acabado con retraso :(

---
+++ src/main/java/com/mycompany/examenfx/Persona.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.examenfx;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Persona {
+    
+    private String nombre;
+    private String sexo;
+    private Integer edad;
+    private Double peso;
+    private Double altura;
+    private String actividad;
+    private Double ger;
+    private Double get;
+    
+    public Persona() {
+    }   
+        
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public String getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
+    }
+
+    public Double getGer() {
+        return ger;
+    }
+
+    public void setGer(Double ger) {
+        this.ger = ger;
+    }
+
+    public Double getGet() {
+        return get;
+    }
+
+    public void setGet(Double get) {
+        this.get = get;
+    }
+    
+    
+    
+    
+}
--- src/main/java/com/mycompany/examenfx/PrimaryController.java
+++ src/main/java/com/mycompany/examenfx/PrimaryController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
@@ -51,15 +52,20 @@ public class PrimaryController implements Initializable {
     private TableColumn<Double, Double> tabla_Ger;
     @FXML
     private TableColumn<Double, Double> Tabla_Get;
+    @FXML
+    private TableView<Persona> tabla;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    ObservableList<String> sexo = FXCollections.observableArrayList();
-    ObservableList<String> actividad = FXCollections.observableArrayList();
-    sexo.addAll("Hombre", "Mujer");
-    actividad.addAll("Ligera", "Muy ligera", "Moderada", "Intensa");
-    actividad.setAll(actividad);
-    sexo.setAll(sexo);
+    ObservableList<String> sexoLista = FXCollections.observableArrayList();
+    ObservableList<String> actividadLista = FXCollections.observableArrayList();
+    
+    sexoLista.addAll("Hombre", "Mujer");
+    actividadLista.addAll("Muy ligera", "Ligera", "Moderada", "Intensa");
+   
+    actividad.setItems(actividadLista);
+    sexo.setItems(sexoLista);
+    
     edad.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 0));
     peso.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 300, 0));
     altura.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 300, 0));
@@ -78,13 +84,98 @@ public class PrimaryController implements Initializable {
     @FXML
     private void update(ActionEvent event) {
       
-        nombre.setText("");
-      
-        
-        
-        
-       
-       
+         if(nombre.getText() == "")
+         {
+             mensaje.setText("El nombre esta vacio");
+         }
+         else if(sexo.valueProperty().get() == null){
+             mensaje.setText("El sexo esta vacio");
+         }
+         else if(edad.valueFactoryProperty().get() == null){
+             mensaje.setText("La edad esta vacia");
+         }
+         else if(peso.valueFactoryProperty().get()==null){
+             mensaje.setText("El peso esta vacio");
+         }
+         else if(altura.valueFactoryProperty().get() == null){
+             mensaje.setText("La altura esta vacia");
+         }
+         else if(actividad.valueProperty().get() == null){
+             mensaje.setText("La actividad esta vacia");
+         }
+         else{
+             mensaje.setText("");
+             
+             Persona paciente = new Persona();
+             
+             paciente.setNombre(nombre.getText());
+             paciente.setSexo(sexo.valueProperty().get());
+             paciente.setEdad(edad.getValue());
+             paciente.setPeso(peso.getValue());
+             paciente.setAltura(altura.getValue());
+             paciente.setActividad(actividad.valueProperty().get());
+             
+             if(paciente.getSexo() == "Hombre" && paciente.getActividad() == "Muy ligera"){
+                 paciente.setGet(1.3);
+             }
+             else if(paciente.getSexo() ==  "Hombre" && paciente.getActividad() == "Ligera")
+             {
+                 paciente.setGet(1.6);
+             }
+             else if(paciente.getSexo() == "Hombre" && paciente.getActividad() == "Moderada")
+             {
+                 paciente.setGet(1.7);
+             }
+             else if(paciente.getSexo() == "Hombre" && paciente.getActividad() == "Intensa")
+             {
+                 paciente.setGet(2.1);
+             }
+             else if(paciente.getSexo() == "Mujer" && paciente.getActividad() == "Muy ligera"){
+                 paciente.setGet(1.3);
+             }
+             else if(paciente.getSexo() ==  "Mujer" && paciente.getActividad() == "Ligera")
+             {
+                 paciente.setGet(1.5);
+             }
+             else if(paciente.getSexo() == "Mujer" && paciente.getActividad() == "Moderada")
+             {
+                 paciente.setGet(1.6);
+             }
+             else if(paciente.getSexo() == "Mujer" && paciente.getActividad() == "Intensa")
+             {
+                 paciente.setGet(1.9);
+             }
+             
+             if(paciente.getSexo() == "Hombre"){
+                 Double temp;
+                 
+                temp = 66.473+13.751*paciente.getPeso()+5.0033*paciente.getAltura()-6.755*paciente.getEdad();
+                
+                paciente.setGer(temp);
+             }
+             else if(paciente.getSexo() == "Mujer"){
+                 Double temp;
+                 
+                temp = 655.0955+9.463*paciente.getPeso()+1.8496*paciente.getAltura()-4.6756*paciente.getEdad();
+                
+                paciente.setGer(temp);
+             }
+             
+                ObservableList<Persona> pacientes = FXCollections.observableArrayList();
+                
+                
+                pacientes.addAll(tabla.getItems());
+                pacientes.add(paciente);
+                
+                tabla.setItems(pacientes);
+                
+             nombre.setText("");
+             sexo.valueProperty().set(null);
+             edad.getValueFactory().setValue(0);
+             peso.getValueFactory().setValue(0.0);
+             altura.getValueFactory().setValue(0.0);
+             actividad.valueProperty().set(null);
+         }
     }
 
 }
